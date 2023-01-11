28683681eddb26d4eef1189ba07db11dc30ed68d
Selimkhanov Deni <selimkhanov_deni@list.ru>
2023-01-11 18:54:54 +0700

    #2 Настройка порта и подключения к базе данных

---
+++ database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-mern-stack', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => console.log('Database connection success'))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = connectDB
